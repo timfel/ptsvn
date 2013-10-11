74b946a989b589ba8612ded760b31a9d00b57604
Tim Felgentreff <timfelgentreff@gmail.com>
2013-10-11 19:10:00 +0200

    this works now

--- auth.rb
+++ auth.rb
@@ -1,3 +1,11 @@
+#! /usr/bin/ruby
+
+LOG = File.open("/tmp/authlog", "a+")
+def log(str)
+  LOG.puts("#{Time.now} #{str}")
+  LOG.flush
+end
+
 SVN_ROOT = File.expand_path("../svn", __FILE__)
 REPO_PATH = "users"
 SVN_HOST = "http://localhost:8443"
@@ -10,17 +18,12 @@ module SvnAccess
     system("svnlook pl #{SVN_ROOT} #{REPO_PATH}/#{path}")
   end
 
-  def mkdir(path)
-    system("svn mkdir #{SVN_HOST}/#{SVN_HOST_PATH}/#{REPO_PATH}/#{path} --non-interactive -n \"Create #{path}\"")
-  end
-
   def cat(path)
     `svnlook cat #{SVN_ROOT} #{REPO_PATH}/#{path}`
   end
 
   def accessible?(user, path)
-    path = path.sub("/users", "")
-
+    log path
     # don't break out of sandbox
     return false unless File.expand_path(path, "/") =~ /^\/#{user}/
 
@@ -33,12 +36,16 @@ module SvnAccess
       if exists?("#{this}/partner")
         name = cat("#{this}/partner").split(/\r|\n/).first
         return false unless name == other
+      else
+        return false
       end
     end
   end
 
   def user_folder?(user, path)
     path_parts = path.split("/").reject(&:empty?)
+    log path_parts
+    log path_parts[0] == user
     path_parts[0] == user
   end
 
@@ -47,11 +54,26 @@ module SvnAccess
   end
 
   def authorize
+    log ENV["USER"].inspect
+    log ENV.inspect
     user = ENV["USER"].downcase
+    return false if user.empty?
     ensure_svn(user)
 
-    return ((ENV["URI"].start_with?(REPO_PATH) && accessible?(user, ENV["URI"])) ||
-            ENV["URI"].start_with?("!svn"))
+    log user
+    log ENV["URI"]
+    path = ENV["URI"].sub("/#{SVN_HOST_PATH}", "")
+    log path
+    if path.start_with?("/!svn")
+      path = "/" + path.split("/")[4..-1].join("/")
+    end
+    log path
+    return true if path == "/"
+    return false unless path.start_with?("/users")
+    path.sub!("/users", "")
+    log path
+
+    return accessible?(user, path)
   end
 end
 
